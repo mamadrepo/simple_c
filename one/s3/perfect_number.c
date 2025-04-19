751578b6a2783db90bf4b475b724285bf75d616c
mamadrepo <nywzhq@gmail.com>
2025-04-19 22:55:37 +0330

    program Finished writing the program

--- one/s3/perfect_number.c
+++ one/s3/perfect_number.c
@@ -2,6 +2,33 @@
 
 int main()
 {
+	int get_number = 0;
+	int perfect_number = 0;
+
+	int i;
+	char ch = 0;
+	
+	do{
+		printf("Enter number: ");
+		scanf("%d", &get_number);
+		
+		for(i = 1; i < get_number; i++){
+			if(get_number % i == 0)
+				perfect_number += i;
+		}
+		if(get_number == perfect_number)
+			printf("Number is perfect\n");
+		else
+			printf("Number is not perfect\n");
+
+		printf("Do you wont continue y/n? ");
+		getchar();
+		ch = getchar();
+		if(ch != 'y')
+			break;
+	}while(1);
+
+		
 
 	return 0;
 }
