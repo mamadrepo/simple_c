077a2a32870526b6593c7551d1a6ce86e1896eeb
mamadrepo <nywzhq@gmail.com>
2025-05-10 19:58:39 +0330

    Create two function for doing works

--- one/s4/price_calculate.c
+++ one/s4/price_calculate.c
@@ -4,8 +4,27 @@
 #define HIGH 0.1
 #define NUM 7
 
+float price(float, int);
+void show_result(float, int, float);
+
 int main()
 {
+	float wholesale;
+	float retail;
+	
+	int times;
 
+	char ch = 'y';
 	return 0;
+}
+
+float price(float wholesale, int times){
+	if(times <= NUM)
+		return (wholesale + wholesale * LOW);
+	return (wholesale + wholesale * HIGH)
+}
+
+void show_result(float wholesale, int times, float retail){
+	printf("Wholesale = %f, Times = %f\n", wholesale, times);
+	printf("Retail = %f\n", retail);
 }
