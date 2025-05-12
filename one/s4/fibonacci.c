3bce8cd9f36f7929b41a5f0364c978eef4377dd1
mamadrepo <nywzhq@gmail.com>
2025-05-12 20:43:46 +0330

    print fibonacci number with recursive function

--- one/s4/fibonacci.c
+++ one/s4/fibonacci.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
 
+void fib(int);
 
 int main()
 {
+	int i;
+	printf("Enter number: ");
+	scanf("%d", &i);
+	fib(i);
 	
 	return 0;
 }
+
+void fib(int i){
+	static int one = 0;
+	static int two = 1;
+	static int sum = 0;
+	if(i > 0)
+		fib(i - 1);
+	
+	printf("%d\n", sum);
+	sum = one + two;
+	one = two;
+	two = sum;
+
+}
+
