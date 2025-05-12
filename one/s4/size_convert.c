92108d4754a487bf758c5b7c01294128f6f9ef24
mamadrepo <nywzhq@gmail.com>
2025-05-12 21:13:13 +0330

    Creating a function to perform the logic of the application

--- one/s4/size_convert.c
+++ one/s4/size_convert.c
@@ -1,7 +1,38 @@
 #include <stdio.h>
 
+void convert(float, int);
+
 int main()
 {
-	
+	printf("[1] -> cm\n");
+	printf("[2] -> meter\n");
+	printf("[3] -> inch\n");
+	printf("[4] -> foot\n");
 	return 0;
+}
+
+void convert(float num, int choose){
+	if(choose == 1){
+		printf("m -> %1.4f\n", num / 100);
+		printf("inch -> %1.4f\n", num / 2.54);
+		printf("foot -> %1.4f\n", num / 30.48);	
+	}
+	else if(choose == 2){
+		float cm = num * 100;
+		printf("cm -> %1.4f\n", cm);
+		printf("inch -> %1.4f\n", cm / 2.54);
+		printf("foot -> %1.4f\n", cm / 30.48);
+	}
+	else if(choose == 3){
+		float cm = num * 2.54;
+		printf("cm -> %1.4f\n", cm);
+		printf("m -> %1.4f\n", cm / 100);
+		printf("foot -> %1.4f\n", cm / 30.48);
+	} 
+	else if(choose == 4){
+		float cm = num * 30.48;
+		printf("cm -> %1.4f\n", cm);
+		printf("m -> %1.4f\n", cm / 100);
+		printf("inch -> %1.4f\n", cm / 2.54);
+	}
 }
