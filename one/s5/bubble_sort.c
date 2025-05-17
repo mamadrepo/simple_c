12dda85f0bfc592dc8eb81120ef5cb99a85c5665
mamadrepo <nywzhq@gmail.com>
2025-05-17 21:08:26 +0330

    bubble function completed

--- one/s5/bubble_sort.c
+++ one/s5/bubble_sort.c
@@ -17,3 +17,18 @@ void get_input(int arr[], int len){
 		scanf("%d", &arr[i]);
 	}
 }
+
+void bubble(int arr[], int len){
+	int i;
+	int j;
+	int item;
+
+	for(i = 0; i < len; i++)
+		for(j = 0; j < len; j++)
+			if(arr[j] > arr[j + 1]){
+				item = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = item;
+			}
+}
+
