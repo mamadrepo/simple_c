184947e1345d0101a73758c39478a3a566ea6845
mamadrepo <nywzhq@gmail.com>
2025-05-19 21:20:58 +0330

    The ID receives a number of students and then receives an id and searches for it

--- one/s5/search_arr.c
+++ one/s5/search_arr.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
+ 
+void get_input(int [], int);
+int search(int [], int, int);
 
 int main()
 {
 
+	return 0;
+}
+
+void get_input(int arr[], int len){
+	for(int i = 0; i < len; i++){
+		printf("Enter student id %i: ", i + 1);
+		scanf("%d", &arr[i]);
+	}
+}
+
+int search(int arr[], int len, int obj){
+	for(int i = 0; i < len; i++)
+		if(arr[i] == obj)
+			return 1;
 	return 0;
 }
