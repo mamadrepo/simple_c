78d96eab90b2b5c0ba00c5aacda384d51502f99b
mamadrepo <nywzhq@gmail.com>
2025-05-19 21:30:11 +0330

    program completed

--- one/s5/search_arr.c
+++ one/s5/search_arr.c
@@ -5,6 +5,20 @@ int search(int [], int, int);
 
 int main()
 {
+	const int size = 5;
+	int arr[size];
+	int obj;
+
+	printf("Enter %i id\n", size);
+	get_input(arr, size);
+
+	printf("Enter id you want search: ");
+	scanf("%d", &obj);
+
+	if(search(arr, size, obj))
+		printf("Id exist!\n");
+	else
+		printf("Id not found\n");
 
 	return 0;
 }
